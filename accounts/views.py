6e0d3be6749a71f88c97f1e3c23c3d69a63caca0
lang96 <1191101295@student.mmu.edu.my>
2022-08-04 15:39:00 +0800

    User accounts

--- accounts/views.py
+++ accounts/views.py
@@ -36,8 +36,8 @@ def register(request):
 
 def login_user(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
 
         user = auth.authenticate(username=username, password=password)
 
