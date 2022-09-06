60b8c1d0b3b662e83fec08653f9831b42847f83b
lang96 <1191101295@student.mmu.edu.my>
2022-09-06 11:03:57 +0800

    Dashboard requiring login

--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
 def home(request):
     return render(request, "home/home.html")
 
-@login_required(login_url='/accounts/login_user/')
 def dashboard(request):
-    return render(request, "home/dashboard.html")
+    if request.user.is_authenticated:
+        return render(request, "home/dashboard.html")
+    else:
+        messages.info(request, 'Please log in to view the dashboard.')
+        return redirect("accounts:login_user")
